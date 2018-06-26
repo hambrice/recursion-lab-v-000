50f0a4ebb47d167054305a3bc69ad3ef16799248
Brad Hambrice <rbh0005@gmail.com>
2018-06-26 01:43:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ function isPalindrome(string) {
   if (string.length <= 1) {
     return true;
   } else if (string[0] === string[string.length - 1]) {
-    isPalindrome(string.substring(1, string.length - 1));
+    let newString = string.substring(1, string.length - 1)
+    isPalindrome(newString);
   } else {
     return false;
   } 
