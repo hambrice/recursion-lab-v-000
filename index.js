18b1175d9cab43abe1ad0e717f9e72b8a80bffab
Brad Hambrice <rbh0005@gmail.com>
2018-06-26 01:20:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,3 +20,11 @@ function reverseString(myString) {
     return myString
   }
 }
+
+function isPalindrome(string) {
+  if (string[0] === string[string.length - 1]) {
+    isPalindrome(string.substring(1, string.length - 1));
+  } elsif (string.length === 1) {
+    return true;
+  }
+}
