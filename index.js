355393410c9adc9bebbe87fca065353ec78b6243
Brad Hambrice <rbh0005@gmail.com>
2018-06-26 01:20:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function reverseString(myString) {
 function isPalindrome(string) {
   if (string[0] === string[string.length - 1]) {
     isPalindrome(string.substring(1, string.length - 1));
-  } elsif (string.length === 1) {
+  } else if (string.length === 1) {
     return true;
   }
 }
