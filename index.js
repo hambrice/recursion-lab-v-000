1d201c2355f45a7a271c5494b3ef13e4e155740f
Brad Hambrice <rbh0005@gmail.com>
2018-06-26 01:29:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ function reverseString(myString) {
 }
 
 function isPalindrome(string) {
-  if (string.length === 1) {
+  if (string[0] === string[string.length - 1] ) {
     return true;
-  } else if (string[0] === string[string.length - 1]) {
+  } else if () {
     isPalindrome(string.substring(1, string.length - 1));
   } else {
     return false;
